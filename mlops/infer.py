3f91cb6d3706f0dca4996b4c362e256e30bbad83
lewinskydmitry <61576391+lewinskydmitry@users.noreply.github.com>
2023-10-19 12:34:00 +0200

    add fire and finish inference

--- mlops/infer.py
+++ mlops/infer.py
@@ -1,24 +1,27 @@
+import fire
 import pandas as pd
 import torch
 from models.basic_model import Baseline_classifier
 
 from mlops.tools.inference_model import infer_model
 
-BATCH_SIZE = 512
 
+class Inferencer:
+    def __init__(self, batch_size):
+        self.batch_size = batch_size
 
-def main():
-    df = pd.read_csv("mlops/data/prepared_data.csv")
-    parameters = torch.load("mlops/saved_models/classifier_model.pth")
-    model = Baseline_classifier(
-        parameters["classifier.0.weight"].shape[1],
-        parameters["classifier.0.weight"].shape[0],
-    )
-    model.load_state_dict(parameters)
+    def make_infer(self):
+        df = pd.read_csv("mlops/data/prepared_data.csv")
+        parameters = torch.load("mlops/saved_models/classifier_model.pth")
+        model = Baseline_classifier(
+            parameters["classifier.0.weight"].shape[1],
+            parameters["classifier.0.weight"].shape[0],
+        )
+        model.load_state_dict(parameters)
 
-    model.eval()
-    infer_model(df, BATCH_SIZE, model)
+        model.eval()
+        infer_model(df, self.batch_size, model)
 
 
 if __name__ == "__main__":
-    main()
+    fire.Fire(Inferencer)
