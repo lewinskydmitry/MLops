81548e93775941981f8360de7929ba7ac61f319c
lewinskydmitry <61576391+lewinskydmitry@users.noreply.github.com>
2023-10-19 14:20:08 +0200

    finish with fire

--- commands.py
+++ commands.py
@@ -10,12 +10,27 @@ def train(
     num_parameters: int = 256,
     num_epoch: int = 10,
 ) -> None:
+    """
+    Train a model.
+
+    Args:
+        path_to_data (str): Path to the training data.
+        batch_size (int): Batch size for training (default: 256).
+        num_parameters (int): Number of model parameters (default: 256).
+        num_epoch (int): Number of training epochs (default: 10).
+    """
     Trainer().train_model(path_to_data, batch_size, num_parameters, num_epoch)
 
 
 def infer(batch_size: int = 256) -> None:
+    """
+    Make inferences using a trained model.
+
+    Args:
+        batch_size (int): Batch size for making inferences (default: 256).
+    """
     Inferencer().make_infer(batch_size)
 
 
 if __name__ == "__main__":
-    fire.Fire()
+    fire.Fire({"train": train, "make": infer})
--- mlops/train.py
+++ mlops/train.py
@@ -5,18 +5,52 @@ from mlops.tools.train_model import train_model
 
 
 class Trainer:
-    def create_model(self, num_features, num_parameters):
+    def create_model(
+        self, num_features: int, num_parameters: int
+    ) -> Baseline_classifier:
+        """
+        Create a baseline classifier model.
+
+        Args:
+            num_features (int): Number of features in the input data.
+            num_parameters (int): Number of parameters for the model.
+
+        Returns:
+            Baseline_classifier: An instance of the baseline classifier model.
+        """
         model = Baseline_classifier(num_features, num_parameters)
         return model
 
-    def load_data(self, path):
+    def load_data(self, path: str) -> pd.DataFrame:
+        """
+        Load data from a CSV file.
+
+        Args:
+            path (str): Path to the CSV file.
+
+        Returns:
+            pd.DataFrame: Loaded DataFrame from the CSV file.
+        """
         df = pd.read_csv(path)
         return df
 
     def train_model(
-        self, path_to_data, batch_size=256, num_parameters=256, num_epoch=10
-    ):
+        self,
+        path_to_data: str,
+        batch_size: int = 256,
+        num_parameters: int = 256,
+        num_epoch: int = 10,
+    ) -> None:
+        """
+        Train a baseline classifier model using the provided data.
+
+        Args:
+            path_to_data (str): Path to the CSV file containing the training data.
+            batch_size (int, optional): Batch size for training (default: 256).
+            num_parameters (int, optional): Number of parameters for the model (default: 256).
+            num_epoch (int, optional): Number of training epochs (default: 10).
+        """
         df = self.load_data(path_to_data)
-        num_features = len(df) - 1
+        num_features = len(df.columns) - 1
         model = self.create_model(num_features, num_parameters)
         train_model(df, num_epoch, batch_size, model)
